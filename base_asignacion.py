f34200c65238eada1c8ff2c6ad65b82720a55c0b
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-09-01 22:07:41 -0500

    Update headers

--- base_asignacion.py
+++ base_asignacion.py
@@ -3,7 +3,7 @@ import pandas as pd
 asignacion_path = 'bases/base_asignacion_20240813.xlsx'
 df_asignacion = pd.read_excel(asignacion_path)
 
-cols_asignacion = ['codigo', 'ID_FLAG', 'cont_18', 'nombre', 'TIPO_CARTERA', 'TIPO_FONDO', 'clave', 'AGENCIA CORRECTA', 'ID_VTA.']
+cols_asignacion = ['codigo', 'ID_FLAG', 'cont_18', 'nombre', 'TIPO_CARTERA', 'TIPO_FONDO', 'clave', 'AGENCIA CORRECTA']
 df_asignacion = df_asignacion[cols_asignacion]
 
 df_asignacion['cont_18'] = df_asignacion['cont_18'].apply(lambda x: str(int(x)).zfill(18) if pd.notna(x) else x)
