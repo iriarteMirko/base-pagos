c23f04f6956a4b49ddca62827ab108a0ca9fb3e5
Mirko <iriartemirko@gmail.com>
2024-11-05 12:17:36 -0500

    Update email test

--- email_test.py
+++ email_test.py
@@ -3,24 +3,30 @@ from dotenv import load_dotenv
 import os
 
 class EmailTest:
-    def __init__(self, fecha: str, file_path: str, hora_formateada: str):
+    def __init__(self, file_path: str,):
         load_dotenv()
-        self.fecha = fecha
         self.file_path = file_path
-        self.hora_formateada = hora_formateada
         self.sender_email = os.getenv('GMAIL')
         self.password = os.getenv('PASSWORD')
-        self.recipients_emails = os.getenv('MULTIPRODUCTO_test').split(', ')
-        self.cc_emails = os.getenv('CC_MULTIPRODUCTO_test').split(', ')
+        self.recipients_emails = os.getenv('RECIPIENTS_TEST').split(', ')
+        self.cc_emails = os.getenv('CC_TEST').split(', ')
         self.gmail_sender = GmailSender(self.sender_email, self.password)
     
     def enviar_correo(self):
-        subject = f'MULTIPRODUCTO {self.fecha}'
+        subject = f'Test email subject'
         body = {
-            'body_msg': 'Multiproducto',
-            'hora': self.hora_formateada,
+            'body_msg': 'Test message',
             'firma': 'Mirko',
         }
-        files = [f'{self.file_path}/MULTIPRODUCTO {self.fecha}.xlsx', f'{self.file_path}/REACTIVA {self.fecha}.xlsx']
+        files = [self.file_path]
         
         self.gmail_sender.send_email(recipient_emails=self.recipients_emails, subject=subject, body=body, cc_emails=self.cc_emails, files=files)
+
+
+def main():
+    file_path = 'C://Users//p042833//Documents//GitHub//base-pagos//src//test//test_file.txt'
+    email_test = EmailTest(file_path)
+    email_test.enviar_correo()
+
+if __name__ == '__main__':
+    main()
