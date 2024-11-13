5416e89f8c44bfc6d420c3b861bc91bd31a64674
Mirko <iriartemirko@gmail.com>
2024-11-13 11:08:20 -0500

    Update multiproducto

--- multiproducto.py
+++ multiproducto.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 import os
 
 class CorreoMultiproducto:
-    def __init__(self, fecha: str, file_path: str, hora_formateada: str):
+    def __init__(self, fecha: str, file_path: str, hora_formateada: str, flag_reactiva: bool):
         load_dotenv()
         self.fecha = fecha
         self.file_path = file_path
         self.hora_formateada = hora_formateada
+        self.flag_reactiva = flag_reactiva
         self.sender_email = os.getenv('GMAIL')
         self.password = os.getenv('PASSWORD')
         self.recipients_emails = os.getenv('MULTIPRODUCTO').split(', ')
@@ -21,6 +22,11 @@ class CorreoMultiproducto:
             'hora': self.hora_formateada,
             'firma': 'Mirko',
         }
-        files = [f'{self.file_path}/MULTIPRODUCTO {self.fecha}.xlsx', f'{self.file_path}/REACTIVA {self.fecha}.xlsx']
+        files = [f'{self.file_path}/MULTIPRODUCTO {self.fecha}.xlsx', ]
+        
+        if self.flag_reactiva:
+            files =+ [f'{self.file_path}/REACTIVA {self.fecha}.xlsx']
+            
+        print('Archivos adjuntos:', len(files))
         
         self.gmail_sender.send_email(recipient_emails=self.recipients_emails, subject=subject, body=body, cc_emails=self.cc_emails, files=files)
